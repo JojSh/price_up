7845e7eb7194686d792ba2219b543055f47e4bb0
Jojo <joseph.sharratt@gmail.com>
2016-05-22 15:56:26 +0100

    Replacing globabl variables with sessions

--- app/server.rb
+++ app/server.rb
@@ -7,6 +7,8 @@ class PriceUpApp < Sinatra::Base
   end
 
   get '/calculator' do
+    @wp_result = session[:wp_result]
+    @rrp_result = session[:rrp_result]
     erb :calculator
   end
 
@@ -15,8 +17,8 @@ class PriceUpApp < Sinatra::Base
     time  = params[:minutes].to_i
     wholesale_price = params[:wholesale_price].to_f
 
-    $wp_result = PriceUp.new.wholesale(costs, time)
-    $rrp_result = PriceUp.new.retail(wholesale_price)
+    session[:wp_result] = PriceUp.new.wholesale(costs, time)
+    session[:rrp_result] = PriceUp.new.retail(wholesale_price)
     redirect '/calculator'
   end
 
